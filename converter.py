8dc60e50bf56ba696ec3f1d53f5fc0682d0b9b76
Muhammad Ali Urakov <106965847+Urakov-Ali@users.noreply.github.com>
2023-01-22 00:12:13 +0500

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,12 +1,12 @@
 import json
 import sqlite3
 
-json_data = open('user_data_1000.json')   
+json_data = open('my_json_file.json')   
 data1 = json.load(json_data)
 
 conn = sqlite3.connect("db.sqlite3")
 c = conn.cursor()
-c.execute("""CREATE TABLE IF NOT EXISTS test(
+c.execute("""CREATE TABLE IF NOT EXISTS my_app_myusers(
         id INTEGER,
         first_name varchar(30),
         last_name varchar(30),
